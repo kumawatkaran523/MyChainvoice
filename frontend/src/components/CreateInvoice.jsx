587f1f5c8a25a209b29c4afeebb1f1552ffc4a5d
kumawatkaran523 <kumawatkaran523@gmail.com>
2025-01-28 23:17:52 +0530

    -

--- frontend/src/components/CreateInvoice.jsx
+++ frontend/src/components/CreateInvoice.jsx
@@ -1,16 +1,28 @@
 import React, { useState } from 'react'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
-import { BrowserProvider, Contract, ethers, JsonRpcProvider} from 'ethers'
+import { BrowserProvider, Contract, ethers, JsonRpcProvider } from 'ethers'
 import { useAccount, useWalletClient } from 'wagmi'
 import { ChainvoiceABI } from '../contractsABI/ChainvoiceABI';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+import { Calendar } from "@/components/ui/calendar"
+import { CalendarIcon } from 'lucide-react'
+import { cn } from '@/lib/utils'
+import { format } from 'date-fns'
 
 function CreateInvoice() {
-  const {data:walletClient} =useWalletClient();
+  const { data: walletClient } = useWalletClient();
   const { isConnected } = useAccount();
   const [receiverAddress, setReceiverAddress] = useState('');
   const [amount, setAmount] = useState('');
   const [loading, setLoading] = useState(false);
+  const account = useAccount();
+  const [dueDate, setDueDate] = useState();
 
   const createInvoiceRequest = async () => {
     if (!isConnected || !walletClient) {
@@ -21,19 +33,19 @@ function CreateInvoice() {
     try {
       setLoading(true);
       const provider = new BrowserProvider(walletClient);
-        // const provider = new JsonRpcProvider(import.meta.env.VITE_BLOCKCHAIN_URI);
+      // const provider = new JsonRpcProvider(import.meta.env.VITE_BLOCKCHAIN_URI);
       const signer = await provider.getSigner();
       const contract = new Contract(
-        import.meta.env.VITE_CONTRACT_ADDRESS, 
-        ChainvoiceABI, 
+        import.meta.env.VITE_CONTRACT_ADDRESS,
+        ChainvoiceABI,
         signer
-      ); 
-      
+      );
+
       console.log(receiverAddress);
       setReceiverAddress('');
       setAmount('');
 
-      const res=await contract.createInvoice(ethers.parseUnits(amount,18),receiverAddress);
+      const res = await contract.createInvoice(ethers.parseUnits(amount, 18), receiverAddress);
       console.log('Transaction details:', {
         from: res.from,
         to: res.to,
@@ -48,27 +60,72 @@ function CreateInvoice() {
     }
   }
 
-  
+
   return (
     <div className='font-Inter'>
       <h2 className="text-lg font-bold mb-7">Create New Invoice Request</h2>
+      <Button>
+            Created At :  
+          <CalendarIcon />
+            {format(new Date(),'PPP')}
+          </Button>
       <div className='grid mx-10'>
-        <Input 
-          type="text" 
-          placeholder='Receiver address' 
+        <Input
+          type="text"
+          placeholder='Sender address'
+          value={`From : ${account.address}`}
+          className='w-1/2 my-2'
+          disabled
+        />
+        <Input
+          type="text"
+          placeholder='Full Name'
+          // value={receiverAddress}
+          // onChange={(e) => setReceiverAddress(e.target.value)}
+          className='w-1/2 my-2'
+        />
+        <div className='flex gap-2 items-center'>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                variant={"outline"}
+                className={cn(
+                  "w-[260px] justify-start text-left font-normal",
+                  !dueDate && "text-muted-foreground"
+                )}
+              >
+                Due Date : 
+                <CalendarIcon />
+                {dueDate ? format(dueDate, "PPP") : <span>Pick a due date</span>}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                mode="single"
+                selected={dueDate}
+                onSelect={setDueDate}
+                initialFocus
+              />
+            </PopoverContent>
+          </Popover>
+
+        </div>
+        <Input
+          type="text"
+          placeholder='Receiver address'
           value={receiverAddress}
           onChange={(e) => setReceiverAddress(e.target.value)}
           className='w-1/2 my-2'
         />
-        <Input 
-          type="number" 
-          placeholder='Amount (ETH)' 
+        <Input
+          type="number"
+          placeholder='Amount (ETH)'
           value={amount}
           onChange={(e) => setAmount(e.target.value)}
           className='w-1/2 my-2'
-        />        
-        <Button 
-          className="w-1/2 my-2" 
+        />
+        <Button
+          className="w-1/2 my-2"
           onClick={createInvoiceRequest}
           disabled={loading || !isConnected}
         >
