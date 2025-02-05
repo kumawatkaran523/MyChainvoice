24200d971ee35b123574684077a521dff22da54e
kumawatkaran523 <kumawatkaran523@gmail.com>
2025-02-05 18:38:47 +0530

    added more field for creation of invoice

--- frontend/src/components/CreateInvoice.jsx
+++ frontend/src/components/CreateInvoice.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
 import { BrowserProvider, Contract, ethers, JsonRpcProvider } from 'ethers'
@@ -15,13 +15,11 @@ import { CalendarIcon } from 'lucide-react'
 import { cn } from '@/lib/utils'
 import { format } from 'date-fns'
 import { Label } from './ui/label'
+import EthCrypto from 'eth-crypto'
 
 function CreateInvoice() {
   const { data: walletClient } = useWalletClient();
   const { isConnected } = useAccount();
-  const [receiverAddress, setReceiverAddress] = useState('');
-  const [amount, setAmount] = useState('');
-  const [loading, setLoading] = useState(false);
   const account = useAccount();
   const [dueDate, setDueDate] = useState();
   const [issueDate, setIssueDate] = useState(new Date());
@@ -42,10 +40,7 @@ function CreateInvoice() {
         ChainvoiceABI,
         signer
       );
-
       console.log(receiverAddress);
-      setReceiverAddress('');
-      setAmount('');
 
       const res = await contract.createInvoice(ethers.parseUnits(amount, 18), receiverAddress);
       console.log('Transaction details:', {
@@ -61,29 +56,37 @@ function CreateInvoice() {
       setLoading(false);
     }
   }
-  const [itemCount, setItemCount] = useState(1);
+
+  const [itemData, setItemData] = useState([{
+    description: '',
+    qty: '',
+    unitPrice: '',
+    discount: '',
+    tax: '',
+    amount: ''
+  }]);
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
-    
-    // user detail
-    const userAddress=formData.get("userAddress");
-    const userFname=formData.get("userFname");
-    const userLname=formData.get("userLname");
-    const userEmail=formData.get("userEmail");
-    const userCountry=formData.get("userCountry");
-    const userCity=formData.get("userCity");
-    const userPostalcode=formData.get("userPostalcode");
 
-    console.log({
-      userAddress,
-      userFname,
-      userLname,
-      userEmail,
-      userCountry,
-      userCity,
-      userPostalcode
-    });
+    // user detail
+    const userAddress = formData.get("userAddress");
+    const userFname = formData.get("userFname");
+    const userLname = formData.get("userLname");
+    const userEmail = formData.get("userEmail");
+    const userCountry = formData.get("userCountry");
+    const userCity = formData.get("userCity");
+    const userPostalcode = formData.get("userPostalcode");
+
+    // client detail
+    const clientAddress = formData.get("clientAddress");
+    const clientFname = formData.get("clientFname");
+    const clientLname = formData.get("clientLname");
+    const clientEmail = formData.get("clientEmail");
+    const clientCountry = formData.get("clientCountry");
+    const clientCity = formData.get("clientCity");
+    const clientPostalcode = formData.get("clientPostalcode");
+
     // Get arrays of all values for each field
     const descriptions = formData.getAll("description");
     const qtys = formData.getAll("qty");
@@ -91,7 +94,7 @@ function CreateInvoice() {
     const discounts = formData.getAll("discount");
     const taxes = formData.getAll("tax");
     const amounts = formData.getAll("amount");
-  
+
     // Combine into array of item objects
     const items = descriptions.map((description, index) => ({
       description,
@@ -101,10 +104,59 @@ function CreateInvoice() {
       tax: taxes[index],
       amount: amounts[index]
     }));
-  
-    console.log(items); // Now contains all items
-    // Proceed with invoice creation...
+    const data = {
+      userAddress,
+      userFname,
+      userLname,
+      userEmail,
+      userCountry,
+      userCity,
+      userPostalcode,
+      clientAddress,
+      clientFname,
+      clientLname,
+      clientEmail,
+      clientCountry,
+      clientCity,
+      clientPostalcode,
+      items
+    }
+    console.log(data);
+
+    e.target.reset();
   };
+
+  const [total, setTotal] = useState(0);
+  const handleItemData = (e, index) => {
+    const { name, value } = e.target;
+
+    setItemData((prevItemData) =>
+      prevItemData.map((item, i) =>
+        i === index ? { ...item, [name]: value } : item
+      )
+    );
+
+  };
+  const addItem = () => {
+    setItemData((prev) => [...prev, {
+      description: '',
+      qty: '',
+      unitPrice: '',
+      discount: '',
+      tax: '',
+      amount: '',
+    }]);
+    console.log(itemData.length);
+
+    
+  }
+  useEffect(() => {
+    let total = itemData.reduce((sum, item) => {
+      return sum + ((parseFloat(item.qty) || 0) * (parseFloat(item.unitPrice) || 0) - (parseFloat(item.discount) || 0) + (parseFloat(item.tax) || 0));
+    }, 0);
+    
+    setTotal(total);
+  }, [itemData]);
   
   return (
     <div className='font-Inter'>
@@ -113,28 +165,16 @@ function CreateInvoice() {
         <Label className='text-lg'>Invoice # </Label>
         <Input value='1' className='w-48' />
         <p> Issued Date </p>
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              variant={"outline"}
-              className={cn(
-                "w-[260px] justify-start text-left font-normal",
-                !issueDate && "text-muted-foreground"
-              )}
-            >
-              <CalendarIcon />
-              {format(issueDate, "PPP")}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0" align="start">
-            <Calendar
-              mode="single"
-              selected={dueDate}
-              onSelect={setDueDate}
-              initialFocus
-            />
-          </PopoverContent>
-        </Popover>
+        <Button
+          variant={"outline"}
+          className={cn(
+            "w-[260px] justify-start text-left font-normal",
+            !issueDate && "text-muted-foreground"
+          )}
+        >
+          <CalendarIcon />
+          {format(issueDate, "PPP")}
+        </Button>
 
         <p> Due Date </p>
         <Popover>
@@ -160,85 +200,86 @@ function CreateInvoice() {
           </PopoverContent>
         </Popover>
       </div>
-      <form  onSubmit={handleSubmit}>
-      <div className='flex space-x-2'>
+      <form onSubmit={handleSubmit}>
+        <div className='flex space-x-2'>
 
-        <div className='border max-w-[550px] p-5 my-3 rounded-md'>
-          <p className='my-2'>From</p>
-          <Input value={account?.address} className='w-[500px]' name="userAddress"/>
-          <div className='mt-4'>
-            <p className='text-gray-500 text-sm my-4'>Add Your Info</p>
-            <div className='flex space-x-4'>
-              <Input type='text' placeholder='Your First Name' className='w-[500px]' name="userFname"/>
-              <Input type='text' placeholder='Your Last Name' className='w-[500px]' name="userLname"/>
-            </div>
+          <div className='border max-w-[550px] p-5 my-3 rounded-md'>
+            <p className='my-2'>From (Your Information)</p>
+            <Input value={account?.address} className='w-[500px]' name="userAddress" />
+            <div className='mt-4'>
+              <p className='text-gray-500 text-sm my-4'>Add Your Info</p>
+              <div className='flex space-x-4'>
+                <Input type='text' placeholder='Your First Name' className='w-[500px]' name="userFname" />
+                <Input type='text' placeholder='Your Last Name' className='w-[500px]' name="userLname" />
+              </div>
 
-            <div className='flex space-x-4 my-5'>
-              <Input type='email' placeholder='Email' className='w-[500px]' name="userEmail"/>
-              <Input type='text' placeholder='Country' className='w-[500px]' name="userCountry"/>
+              <div className='flex space-x-4 my-5'>
+                <Input type='email' placeholder='Email' className='w-[500px]' name="userEmail" />
+                <Input type='text' placeholder='Country' className='w-[500px]' name="userCountry" />
+              </div>
+              <div className='flex space-x-4 my-5'>
+                <Input type='text' placeholder='City' className='w-[500px]' name="userCity" />
+                <Input type='text' placeholder='Postal Code' className='w-[500px]' name="userPostalcode" />
+              </div>
             </div>
-            <div className='flex space-x-4 my-5'>
-              <Input type='text' placeholder='City' className='w-[500px]' name="userCity"/>
-              <Input type='text' placeholder='Postal Code' className='w-[500px]' name="userPostalcode"/>
+          </div>
+
+          <div className='border max-w-[550px] p-5 my-3 rounded-md'>
+            <p className='my-2'>Client Information</p>
+            <Input placeholder='Client Wallet Address' className='w-[500px]' name="clientAddress" />
+            <div className='mt-4'>
+              <p className='text-gray-500 text-sm my-4'>Add Client Info</p>
+              <div className='flex space-x-4'>
+                <Input type='text' placeholder='Client First Name' className='w-[500px]' name="clientFname" />
+                <Input type='text' placeholder='Client Last Name' className='w-[500px]' name="clientLname" />
+              </div>
+
+              <div className='flex space-x-4 my-5'>
+                <Input type='email' placeholder='Email' className='w-[500px]' name="clientEmail" />
+                <Input type='text' placeholder='Country' className='w-[500px]' name="clientCountry" />
+              </div>
+              <div className='flex space-x-4 my-5'>
+                <Input type='text' placeholder='City' className='w-[500px]' name="clientCity" />
+                <Input type='text' placeholder='Postal Code' className='w-[500px]' name="clientPostalcode" />
+              </div>
             </div>
           </div>
         </div>
 
-        <div className='border max-w-[550px] p-5 my-3 rounded-md'>
-          <p className='my-2'>Client Information</p>
-          <Input placeholder='Client Wallet Address' className='w-[500px]' name="clientAddress" />
-          <div className='mt-4'>
-            <p className='text-gray-500 text-sm my-4'>Add Client Info</p>
-            <div className='flex space-x-4'>
-              <Input type='text' placeholder='Client First Name' className='w-[500px]' name="clientFname"/>
-              <Input type='text' placeholder='Client Last Name' className='w-[500px]' name="clientLname"/>
-            </div>
-
-            <div className='flex space-x-4 my-5'>
-              <Input type='email' placeholder='Email' className='w-[500px]' name="clientEmail"/>
-              <Input type='text' placeholder='Country' className='w-[500px]' name="clientCountry"/>
-            </div>
-            <div className='flex space-x-4 my-5'>
-              <Input type='text' placeholder='City' className='w-[500px]' name="clientCity"/>
-              <Input type='text' placeholder='Postal Code' className='w-[500px]' name="clientPostalcode"/>
-            </div>
+        <div className=''>
+          <div className='grid grid-cols-12 justify-center text-center bg-green-500 py-3 rounded-t-md '>
+            <div className='col-span-4 '>DESCRIPTION</div>
+            <div className='col-span-1 '>QTY</div>
+            <div className='col-span-2 '>UNIT PRICE</div>
+            <div className='col-span-1 '>DISCOUNT</div>
+            <div className='col-span-1 '>TAX(%)</div>
+            <div className='col-span-3 '>AMOUNT</div>
           </div>
-        </div>
-      </div>
-
-      <div className=''>
-        <div className='grid grid-cols-12 justify-center text-center bg-green-500 py-3 rounded-t-md '>
-          <div className='col-span-4 '>DESCRIPTION</div>
-          <div className='col-span-1 '>QTY</div>
-          <div className='col-span-2 '>UNIT PRICE</div>
-          <div className='col-span-1 '>DISCOUNT</div>
-          <div className='col-span-1 '>TAX(%)</div>
-          <div className='col-span-3 '>AMOUNT</div>
-        </div>
           <div className="border p-3 rounded-b-md" >
             {
-              Array(itemCount)
-                .fill(null)
+              itemData
                 .map((_, index) => (
-                  <div className="grid grid-cols-12 gap-3 my-2" key={index}>
-                    <Input type="text" placeholder="Enter Description" className="col-span-4 py-5" name="description"/>
-                    <Input type="number" placeholder="0" className="col-span-1 py-5" name="qty"/>
-                    <Input type="text" placeholder="0" className="col-span-2 py-5" name="unitPrice"/>
-                    <Input type="text" placeholder="0" className="col-span-1 py-5" name="discount"/>
-                    <Input type="text" placeholder="0" className="col-span-1 py-5" name="tax"/>
-                    <Input type="text" placeholder="0.00" className="col-span-3 py-5" name="amount"/>
+                  <div className="grid grid-cols-12 gap-3 my-2" key={index} >
+                    <Input type="text" placeholder="Enter Description" className="col-span-4 py-5" name="description" onChange={(e) => handleItemData(e, index)} />
+                    <Input type="number" placeholder="0" className="col-span-1 py-5" name="qty" onChange={(e) => handleItemData(e, index)} />
+                    <Input type="text" placeholder="0" className="col-span-2 py-5" name="unitPrice" onChange={(e) => handleItemData(e, index)} />
+                    <Input type="text" placeholder="0" className="col-span-1 py-5" name="discount" onChange={(e) => handleItemData(e, index)} />
+                    <Input type="text" placeholder="0" className="col-span-1 py-5" name="tax" onChange={(e) => handleItemData(e, index)} />
+                    <Input type="text" placeholder="0.00" className="col-span-3 py-5" name="amount" disabled value={
+                      (parseFloat(itemData[index].qty) || 0) * (parseFloat(itemData[index].unitPrice) || 0) - (parseFloat(itemData[index].discount) || 0) + (parseFloat(itemData[index].tax) || 0)
+                    }
+                    />
                   </div>
                 ))
             }
           </div>
-        <div className='flex justify-between items-center'>
-          <Button className='my-2' onClick={() => {
-            setItemCount(itemCount + 1);
-          }}  type="button"> <span className='text-xl'>+</span> Add Item</Button>
-          <Button className='mx-7' type="submit">Create Invoice</Button>
+          <p className='text-right mr-20'>Total : {total}</p>
+          <div className='flex justify-between items-center'>
+            <Button className='my-2' onClick={addItem} type="button"> <span className='text-xl'>+</span> Add Item</Button>
+            <Button className='mx-7' type="submit">Create Invoice</Button>
+          </div>
         </div>
-      </div>
-          </form>
+      </form>
     </div>
   )
 }
